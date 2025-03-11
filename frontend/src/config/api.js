2b984dfd7376eb9d43e96009741b89532e7657f3
Madhu Bikram Budha Magar <167270782+madhubikram@users.noreply.github.com>
2025-03-11 11:37:04 +0545

    Fixed the authentication issue which was arrising in production port /4173 had to modify api.js file from src\config folder

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,4 +1,4 @@
 const API_URL = import.meta.env.PROD 
-  ? 'http://192.168.1.70:5000' 
-  : 'http://localhost:5000'     
+  ? `${window.location.protocol}//${window.location.hostname}:5000`
+  : 'http://localhost:5000'
 export default API_URL
